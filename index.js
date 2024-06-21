f51cd79990dc6150331a225a6d407416ceafdbf7
WillianBFerreira <wferreira25@hotmail.com>
2024-06-21 14:43:19 -0300

    Projeto felipão

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log ("hello");
+let heroi = ["Kratos" , 9001];
+
+let lvl;
+
+if(heroi [1] >5000){
+    if(heroi [1] >= 10000){
+        lvl = "Radiante";
+    } else if (heroi [1] >= 5001 && heroi [1] <= 7000) {
+        lvl = "Ouro";
+    } else if (heroi [1] >= 7001 && heroi [1] <= 8000) {
+        lvl = "Platina";
+    } else if (heroi [1] >= 8001 && heroi [1] <= 9000) {
+        lvl = "Ascendente";
+    } else if (heroi [1] >= 9001 && heroi [1] <= 10000)
+        lvl = "Imortal";
+
+} else if(heroi [1] < 1000) {
+    lvl = "Ferro";
+} else if (heroi [1] >= 1001 && heroi [1] <= 2000) {
+    lvl = "Bronze";
+} else if (heroi [1] >= 2001 && heroi [1] <= 5000) {
+    lvl = "Prata";
+}
+
+console.log(`O Herói de nome ${heroi [0]} está no nível de ${lvl}.`);
